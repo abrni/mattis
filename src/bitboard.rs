e25cb2269822fcf3139864757f4f37e28120ea1c
abrni <45497598+abrni@users.noreply.github.com>
2024-03-02 11:49:16 +0100

    Add lots of static Bitboards

--- src/bitboard.rs
+++ src/bitboard.rs
@@ -1,7 +1,7 @@
-use std::fmt::Debug;
+use std::fmt::{Debug, Display};
 
-use num_enum::FromPrimitive;
-use crate::types::Square64;
+use num_enum::{FromPrimitive, UnsafeFromPrimitive};
+use crate::types::{Square64, Rank, File};
 
 #[derive(Debug, PartialEq, Eq, Clone, Copy, Hash)]
 pub struct BitBoard(u64);
@@ -9,6 +9,14 @@ pub struct BitBoard(u64);
 impl BitBoard {
     pub const EMPTY: Self = Self(0);
     pub const FULL: Self = Self(u64::MAX);
+
+    pub fn from_u64(v: u64) -> Self {
+        Self(v)
+    }
+
+    pub fn to_u64(self) -> u64 {
+        self.0
+    }
 
     pub const fn is_empty(self) -> bool {
         self.0 == 0
@@ -28,6 +36,10 @@ impl BitBoard {
 
     pub const fn complement(self) -> Self {
         Self(!self.0)
+    }
+
+    pub const fn without(self, other: Self) -> Self {
+        Self(self.0 & !other.0)
     }
 
     pub fn set(&mut self, idx: Square64) {
@@ -89,7 +101,176 @@ impl BitBoard {
         let idx = MAGIC_TABLE[(fold.wrapping_mul(0x783a9b23) >> 26)  as usize];
         Square64::from_primitive(idx)
     }
+
+    pub fn bit_count(self) -> u32 {
+        self.0.count_ones()
+    }
 }
+
+impl Display for BitBoard {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        for rank in (0..8).rev() {
+            let rank = unsafe { Rank::unchecked_transmute_from(rank) };
+            for file in 0..8 {
+                let file = unsafe { File::unchecked_transmute_from(file) };
+                let sq = Square64::from_file_rank(file, rank);
+
+                write!(f, "{} ", if self.get(sq) {"X"} else {"."})?;
+            }
+
+            writeln!(f)?;
+        }
+
+        Ok(())
+    }
+}
+
+// ---------------------------------------------------------------------------------------------------------------------
+// ---------------------------------------------------------------------------------------------------------------------
+// # CONST BITBOARD TABLES ---------------------------------------------------------------------------------------------
+// ---------------------------------------------------------------------------------------------------------------------
+// ---------------------------------------------------------------------------------------------------------------------
+
+
+lazy_static::lazy_static! {
+    pub static ref FILE_BITBOARDS: [BitBoard; 8] = {
+        let mut boards = [BitBoard::EMPTY; 8];
+
+        for f in File::iter_all() {
+            for r in Rank::iter_all() {
+                boards[f].set(Square64::from_file_rank(f, r));
+            }
+        }
+
+        boards
+    };
+
+    pub static ref RANK_BITBOARDS: [BitBoard; 8] = {
+        let mut boards = [BitBoard::EMPTY; 8];
+
+        for r in Rank::iter_all() {
+            for f in File::iter_all() {
+                boards[r].set(Square64::from_file_rank(f, r));
+            }
+        }
+
+        boards
+    };
+
+    pub static ref BORDER: BitBoard = {
+        FILE_BITBOARDS[File::A]
+            .union(FILE_BITBOARDS[File::H])
+            .union(RANK_BITBOARDS[Rank::R1])
+            .union(RANK_BITBOARDS[Rank::R8])
+    };
+
+    pub static ref ROOK_MOVE_PATTERNS: [BitBoard; 64] = {
+        let mut boards = [BitBoard::EMPTY; 64];
+
+        for (i, m) in boards.iter_mut().enumerate() {
+            let mut result = BitBoard::EMPTY;
+            let square = Square64::from_primitive(i);
+            let rank = square.rank().unwrap();
+            let file = square.file().unwrap();
+
+            if let Some(r) = rank.up() {
+                for r in Rank::range_inclusive(r, Rank::R8) {
+                    result.set(Square64::from_file_rank(file, r));
+                }
+            }
+
+            if let Some(r) = rank.down() {
+                for r in Rank::range_inclusive(Rank::R1, r) {
+                    result.set(Square64::from_file_rank(file, r));
+                }
+            }
+
+            if let Some(f) = file.up() {
+                for f in File::range_inclusive(f, File::H) {
+                    result.set(Square64::from_file_rank(f, rank));
+                }
+            }
+
+            if let Some(f) = file.down() {
+                for f in File::range_inclusive(File::A, f) {
+                    result.set(Square64::from_file_rank(f, rank));
+                }
+            }
+
+            *m = result;
+        }
+
+        boards
+    };
+
+    pub static ref BISHOP_MOVE_PATTERNS: [BitBoard; 64] = {
+        let mut boards = [BitBoard::EMPTY; 64];
+
+        for (i, m) in boards.iter_mut().enumerate() {
+            let mut result = BitBoard::EMPTY;
+            let square = Square64::from_primitive(i);
+            let rank = square.rank().unwrap();
+            let file = square.file().unwrap();
+
+            if let Some((r, f)) = rank.up().zip(file.up()) {
+                for (r, f) in std::iter::zip(Rank::range_inclusive(r, Rank::R8), File::range_inclusive(f, File::H)) {
+                    result.set(Square64::from_file_rank(f, r));
+                }
+            }
+
+            if let Some((r, f)) = rank.up().zip(file.down()) {
+                for (r, f) in std::iter::zip(Rank::range_inclusive(r, Rank::R8), File::range_inclusive(File::A, f).rev()) {
+                    result.set(Square64::from_file_rank(f, r));
+                }
+            }
+
+            if let Some((r, f)) = rank.down().zip(file.up()) {
+                for (r, f) in std::iter::zip(Rank::range_inclusive(Rank::R1, r).rev(), File::range_inclusive(f, File::H)) {
+                    result.set(Square64::from_file_rank(f, r));
+                }
+            }
+
+            if let Some((r, f)) = rank.down().zip(file.down()) {
+                for (r, f) in std::iter::zip(Rank::range_inclusive(Rank::R1, r).rev(), File::range_inclusive(File::A, f).rev()) {
+                    result.set(Square64::from_file_rank(f, r));
+                }
+            }
+
+            *m = result;
+        }
+
+        boards
+    };
+
+
+    pub static ref ROOK_MAGIC_MASKS: [BitBoard; 64] = {
+        let mut masks = *ROOK_MOVE_PATTERNS;
+
+        for m in masks.iter_mut() {
+            *m = m.without(*BORDER);
+        }
+
+        masks
+    };
+
+    pub static ref BISHOP_MAGIC_MASKS: [BitBoard; 64] = {
+        let mut masks = *BISHOP_MOVE_PATTERNS;
+
+        for m in masks.iter_mut() {
+            *m = m.without(*BORDER);
+        }
+
+        masks
+    };
+}
+
+
+
+// ---------------------------------------------------------------------------------------------------------------------
+// ---------------------------------------------------------------------------------------------------------------------
+// # TESTS -------------------------------------------------------------------------------------------------------------
+// ---------------------------------------------------------------------------------------------------------------------
+// ---------------------------------------------------------------------------------------------------------------------
 
 #[cfg(test)]
 mod tests {
