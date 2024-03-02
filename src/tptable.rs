c21952bad8700d9b7a0e3458d9388a953dc3fcc9
abrni <45497598+abrni@users.noreply.github.com>
2024-03-02 11:49:18 +0100

    Collect usage data in TpTable

--- src/tptable.rs
+++ src/tptable.rs
@@ -9,7 +9,9 @@ pub struct TpEntry {
 
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub struct TpTable {
-    pub vec: Vec<TpEntry>,
+    vec: Vec<TpEntry>,
+    collisions: usize,
+    filled: usize,
 }
 
 impl TpTable {
@@ -17,16 +19,21 @@ impl TpTable {
         let size_b = 112 << 20; // size in bytes
         let len = size_b / std::mem::size_of::<TpEntry>();
 
+        let vec = vec![
+            TpEntry {
+                m: Move16::build().finish(),
+                key: [0; 5],
+            };
+            len
+        ];
+
         assert!(len <= (1 << 24));
+        assert_eq!(vec.len(), vec.capacity());
 
         Self {
-            vec: vec![
-                TpEntry {
-                    m: Move16::build().finish(),
-                    key: [0; 5]
-                };
-                len
-            ],
+            vec,
+            collisions: 0,
+            filled: 0,
         }
     }
 
@@ -34,6 +41,12 @@ impl TpTable {
         let idx = (key & 0xFFFFFF) as usize;
         let entry = unsafe { self.vec.get_unchecked_mut(idx) };
         let bytes = (key >> 24).to_le_bytes();
+
+        if entry.key == [0, 0, 0, 0, 0] {
+            self.filled += 1;
+        } else if entry.key != bytes[0..5] {
+            self.collisions += 1;
+        }
 
         entry.key.copy_from_slice(&bytes[0..5]);
         entry.m = m;
@@ -49,6 +62,26 @@ impl TpTable {
         } else {
             None
         }
+    }
+
+    pub fn len(&self) -> usize {
+        self.filled
+    }
+
+    pub fn is_empty(&self) -> bool {
+        self.filled == 0
+    }
+
+    pub fn capacity(&self) -> usize {
+        self.vec.len()
+    }
+
+    pub fn collisions(&self) -> usize {
+        self.collisions
+    }
+
+    pub fn fill_level(&self) -> f32 {
+        self.len() as f32 / self.capacity() as f32
     }
 }
 
