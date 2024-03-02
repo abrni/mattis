f9b452db3a063ab3767040a61e689a4121800eba
abrni <45497598+abrni@users.noreply.github.com>
2024-03-02 11:49:19 +0100

    Add support for multiple uci command including basic search

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,19 @@
 use std::io::{BufRead, BufReader};
 
-use mattis::{board::Board, uci::GuiMessage};
+use mattis::{
+    board::Board,
+    moves::Move16,
+    search::{iterative_deepening, SearchParams},
+    tptable::TpTable,
+    uci::{self, EngineMessage, GuiMessage, Id},
+};
 
 const FEN_STARTPOS: &str = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
 
 fn main() {
     let mut board = Board::from_fen(FEN_STARTPOS).unwrap();
     let mut stdin = BufReader::new(std::io::stdin());
+    let mut tptable = TpTable::new();
     let mut input = String::new();
 
     loop {
@@ -16,6 +23,88 @@ fn main() {
             .expect("Could not read input line");
 
         let message = GuiMessage::parse(&input).expect("Received unknown command");
-        dbg!(message);
+
+        match message {
+            GuiMessage::Uci => print_uci_info(),
+            GuiMessage::Isready => println!("{}", EngineMessage::Readyok),
+            GuiMessage::Position { pos, moves } => setup_position(&mut board, pos, &moves),
+            GuiMessage::Go(go) => run_go(&mut board, go, &mut tptable),
+            _ => println!("This uci command is currently not supported."),
+        }
+    }
+}
+
+fn run_go(board: &mut Board, _go: uci::Go, tptable: &mut TpTable) {
+    let params = SearchParams {
+        max_time: None,
+        max_nodes: None,
+        max_depth: Some(5),
+    };
+
+    for stats in iterative_deepening(board, params, tptable) {
+        let info = EngineMessage::Info(uci::Info {
+            depth: Some(stats.depth),
+            nodes: Some(stats.nodes as u32),
+            pv: stats.pv.into_iter().map(|m| format!("{m}")).collect(),
+            score: Some(uci::Score::Cp(stats.score)),
+            string: Some(format!(
+                "fhf/fh = {}/{}={:.2}",
+                stats.fhf,
+                stats.fh,
+                stats.fhf as f64 / stats.fh as f64
+            )),
+            ..Default::default()
+        });
+
+        println!("{info}");
+    }
+
+    let hashfull = tptable.len() as f64 / tptable.capacity() as f64;
+    let hashfull = (hashfull * 1000.0) as u32;
+    let info = EngineMessage::Info(uci::Info {
+        hashfull: Some(hashfull),
+        ..Default::default()
+    });
+
+    println!("{info}");
+
+    let bestmove = tptable.get(board.position_key).unwrap();
+    let bestmove = EngineMessage::Bestmove {
+        move_: format!("{bestmove}"),
+        ponder: None,
+    };
+
+    println!("{bestmove}");
+}
+
+fn print_uci_info() {
+    let name_msg: EngineMessage = EngineMessage::Id(Id::Name("Mattis".to_string()));
+    let author_msg: EngineMessage = EngineMessage::Id(Id::Author("Anton Bornhoeft".to_string()));
+
+    println!("{name_msg}",);
+    println!("{author_msg}");
+    println!("{}", EngineMessage::Uciok);
+}
+
+fn setup_position(board: &mut Board, pos: uci::Position, moves: &[String]) {
+    let fen = match &pos {
+        uci::Position::Fen(fen) => fen,
+        uci::Position::Startpos => FEN_STARTPOS,
+    };
+
+    *board = Board::from_fen(fen).unwrap();
+
+    'outer: for m in moves {
+        let mut movelist = vec![];
+        board.generate_all_moves(&mut movelist);
+
+        for bm in movelist {
+            if (format!("{bm}")) == *m {
+                board.make_move(bm);
+                continue 'outer;
+            }
+        }
+
+        panic!("Invalid move");
     }
 }
--- src/search.rs
+++ src/search.rs
@@ -1,29 +1,83 @@
-use crate::{board::Board, eval::evaluation, tptable::TpTable};
+use crate::{board::Board, eval::evaluation, moves::Move32, tptable::TpTable};
+use std::time::{Duration, Instant};
+
+pub struct SearchParams {
+    pub max_time: Option<Duration>,
+    pub max_nodes: Option<u64>,
+    pub max_depth: Option<u32>,
+    // TODO: Support for Mate Search
+}
 
 #[derive(Debug, PartialEq, Eq, Clone, Default)]
 pub struct SearchStats {
-    pub nodes: usize,     //  total count of visited nodes
-    pub leaves: usize,    // total count of visited leaf nodes
-    pub fh: usize,        // counts fail-highs (beta cut off)
-    pub fhf: usize,       // counts fail-highs at the first move
-    pub tptable: TpTable, // Transposition table
+    pub depth: u32,      // Search depth
+    pub score: i32,      // Score in centipawns
+    pub nodes: u64,      // Total count of visited nodes
+    pub leaves: u64,     // Total count of visited leaf nodes
+    pub fh: u64,         // Count of fail-highs (beta cut off)
+    pub fhf: u64,        // Count of fail-highs at the first move
+    pub pv: Vec<Move32>, // Principle Variation Line
 }
 
-pub fn iterative_deepening(board: &mut Board, depth: usize, stats: &mut SearchStats) -> i32 {
-    let mut score = 0;
-    for i in 1..=depth {
-        score = alpha_beta(-i32::MAX, i32::MAX, i, board, stats);
+pub fn pv_line(tptable: &TpTable, board: &mut Board) -> Vec<Move32> {
+    let mut pvline = Vec::with_capacity(8);
+
+    while let Some(m16) = tptable.get(board.position_key) {
+        let m32 = board.move_16_to_32(m16);
+        board.make_move(m32);
+        pvline.push(m32);
     }
 
-    score
+    for _ in 0..pvline.len() {
+        board.take_move();
+    }
+
+    pvline
+}
+
+pub fn iterative_deepening<'a>(
+    board: &'a mut Board,
+    params: SearchParams,
+    tptable: &'a mut TpTable,
+) -> impl Iterator<Item = SearchStats> + 'a {
+    let max_depth = params.max_depth.unwrap_or(u32::MAX);
+    let max_nodes = params.max_nodes.unwrap_or(u64::MAX);
+    let end_time = params.max_time.map(|t| Instant::now() + t);
+
+    let mut stats = SearchStats::default();
+
+    std::iter::from_fn(move || {
+        stats.depth += 1;
+
+        if stats.depth > max_depth {
+            return None;
+        }
+
+        if stats.nodes > max_nodes {
+            return None;
+        }
+
+        if let Some(t) = end_time {
+            if Instant::now() > t {
+                return None;
+            }
+        }
+
+        let score = alpha_beta(-i32::MAX, i32::MAX, stats.depth, board, &mut stats, tptable);
+        stats.score = score;
+        stats.pv = pv_line(tptable, board);
+
+        Some(stats.clone())
+    })
 }
 
 pub fn alpha_beta(
     mut alpha: i32,
     beta: i32,
-    depth: usize,
+    depth: u32,
     board: &mut Board,
     stats: &mut SearchStats,
+    tptable: &mut TpTable,
 ) -> i32 {
     stats.nodes += 1;
 
@@ -35,27 +89,29 @@ pub fn alpha_beta(
     let mut moves = Vec::with_capacity(32); // TODO: reuse a preallocated vec
     board.generate_all_moves(&mut moves);
 
-    if let Some(m) = stats.tptable.get(board.position_key) {
+    if let Some(m) = tptable.get(board.position_key) {
         let pos = moves.iter().position(|m32| m32.m16 == m).unwrap();
         moves.swap(0, pos);
     }
 
     let mut new_best_move = None;
+    let mut legal_moves = 0;
 
-    for (i, m) in moves.into_iter().enumerate() {
+    for m in moves.into_iter() {
         let is_valid_move = board.make_move(m);
 
         if !is_valid_move {
             continue;
         }
 
-        let score = -alpha_beta(-beta, -alpha, depth - 1, board, stats);
+        legal_moves += 1;
+        let score = -alpha_beta(-beta, -alpha, depth - 1, board, stats, tptable);
         board.take_move();
 
         if score >= beta {
             stats.fh += 1;
 
-            if i == 0 {
+            if legal_moves == 1 {
                 stats.fhf += 1
             };
 
@@ -68,8 +124,16 @@ pub fn alpha_beta(
         }
     }
 
+    if legal_moves == 0 {
+        if board.is_square_attacked(board.king_square[board.color], board.color.flipped()) {
+            return -30_000;
+        } else {
+            return 0;
+        }
+    }
+
     if let Some(m) = new_best_move {
-        stats.tptable.insert(board.position_key, m.m16);
+        tptable.insert(board.position_key, m.m16);
     }
 
     alpha
