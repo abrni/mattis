5d96b44dbebda491b8b547a3a1dec37b1285a07f
abrni <45497598+abrni@users.noreply.github.com>
2024-03-04 14:55:05 +0100

    Add lots of comments to the  alpha-beta function

--- src/search.rs
+++ src/search.rs
@@ -189,6 +189,8 @@ pub fn alpha_beta(
         return 0;
     }
 
+    // We frequently check, if the search should stop
+    // (e.g. because of time running out or a gui command).
     if stats.nodes.trailing_zeros() == 10 {
         stats.stop = should_search_stop(params, stats);
     }
@@ -200,14 +202,24 @@ pub fn alpha_beta(
 
     stats.nodes += 1;
 
+    // Check if we reached a draw by fifty move rule or 3-fold-repetition.
+    // We actually evaluate a single repetition as a draw, so we can find
+    // drawn positions earlier.
     if board.ply >= 1 && (board.is_repetition() || board.fifty_move >= 100) {
         return 0;
     }
 
+    // We extend the depth, if we are in check. This increases the chance to
+    // properly evaluate, whether we are able to get out of check or not.
+    // Even though we handle being in check in the quiescence search, this still
+    // seems to yield positive results.
     if board.in_check() {
         depth += 1;
     }
 
+    // Probe the transposition table. There a two kinds of hashtable hits:
+    // A CutOff-Hit allows us to safely perform a branch cutoff and return early.
+    // Otherwise we can still use the table hit for move ordering.
     let hashtable_probe = tables.transposition_table.probe(board, alpha, beta, depth);
     let pv_move = match hashtable_probe {
         Probe::NoHit => None,
@@ -215,32 +227,40 @@ pub fn alpha_beta(
         Probe::CutOff(_, score) => return score,
     };
 
+    // Null move pruning optimization.
+    // We do a nothing move (passing move) and see if we are still much better than the oponent (by causing a beta cutoff).
+    // In that case we can be sure to have found a good position and return early.
+    // We don't want null move pruning, if we are in check, because that would cause an illegal position.
     if allow_null_move && !board.in_check() && board.ply != 0 && board.count_big_pieces[board.color] > 1 && depth >= 4 {
         board.make_null_move();
         let score = -alpha_beta(-beta, -beta + 1, depth - 4, board, params, stats, tables, false);
         board.take_null_move();
 
+        // Don't use the results, if we entered stop-mode in the meantime.
         if stats.stop {
             return 0;
         }
 
+        // A score above 29_000 probably signals a mate. That result was likely caused by a zugzwang, that wouldn't have
+        // occured without the null move. Do not use the result in that case.
         if score >= beta && score.abs() < 29_000 {
             return beta;
         }
     }
 
-    let mut moves = MoveList::with_capacity(64);
+    let mut moves = MoveList::default();
     board.generate_all_moves(&mut moves);
 
-    let mut best_move = Move32::default();
-    let mut best_score = -30_000;
-    let mut legal_moves = 0;
-    let mut alpha_changed = false;
+    let mut best_move = Move32::default(); // Will contain the best move we found during the search.
+    let mut best_score = -30_000; // TODO: do we really need this?
+    let mut legal_moves = 0; // Counts the number of legal moves. Not every generated move is necessarily legal.
+    let mut alpha_changed = false; // signals if alpha has changed during the evaluation of each move
 
     while let Some(m) = take_next_move(&mut moves, pv_move, tables, board) {
-        let is_valid_move = board.make_move(m);
+        let is_legal_move = board.make_move(m);
 
-        if !is_valid_move {
+        // The move might have been illegal. in that case the move was not made and we can skip to the next one.
+        if !is_legal_move {
             continue;
         }
 
@@ -248,22 +268,30 @@ pub fn alpha_beta(
         let score = -alpha_beta(-beta, -alpha, depth - 1, board, params, stats, tables, true);
         board.take_move();
 
+        // Don't use the result of alpha-beta if we entered stop-mode in the meantime. The result is probably nonsense.
+        // Instead just return ASAP.
         if stats.stop {
             return 0;
         }
 
+        // A score higher than beta allows us to perform a beta cutoff (fail-high)
         if score >= beta {
-            stats.fh += 1;
+            stats.fh += 1; // Track that number of fail-highs
 
+            // Track, if we caused the fail-high on the first legal move
             if legal_moves == 1 {
                 stats.fhf += 1;
             };
 
+            // A quiet move, that caused a beta-cutoff is labeled a 'killer-move'.
+            // If the same move is encountered at the same ply but in a different position, it will be
+            // prefered by move ordering. We use two killer slots, to not forget good moves in some situations.
             if !m.m16.is_capture() {
                 tables.search_killers[board.ply][1] = tables.search_killers[board.ply][0];
                 tables.search_killers[board.ply][0] = m;
             }
 
+            // Store the move in the hashtable and mark it as a beta-cutoff
             tables
                 .transposition_table
                 .store(board.position_key, beta, m.m16, depth, HEKind::Beta);
@@ -275,9 +303,13 @@ pub fn alpha_beta(
             alpha = score;
             alpha_changed = true;
 
+            // If we improved alpha with this move, we increase a corresponding score in our history.
+            // This helps move ordering by prefering moves that are similar to moves which caused alpha improvements before.
+            // TODO: I am not sure, we are doing this right. I should test not using the history heuristic or using a
+            // different added value.
             if !m.m16.is_capture() {
                 let piece = board.pieces[m.m16.start()].unwrap();
-                tables.search_history[piece][m.m16.end()] += depth as u32;
+                tables.search_history[piece][m.m16.end()] += depth as u32; // TODO: is this better: += depth * depth or 2^depth?
             }
         }
 
@@ -287,6 +319,7 @@ pub fn alpha_beta(
         }
     }
 
+    // If we haven't found any legal move, we are either in checkmate or in a stalemate.
     if legal_moves == 0 {
         if board.in_check() {
             return -30_000 + board.ply as i16;
@@ -295,8 +328,11 @@ pub fn alpha_beta(
         return 0;
     }
 
+    // Store the best move we found in the hashtable.
+    // If we have not improved alpha, we mark the best move as an alpha-cutoff.
+    // Otherwise we can return the exact score.
     let hashentry_kind = if alpha_changed { HEKind::Exact } else { HEKind::Alpha };
-    let score = if alpha_changed { best_score } else { alpha };
+    let score = if alpha_changed { best_score } else { alpha }; // TODO: I think, weh should be able to always use alpha here?
     tables
         .transposition_table
         .store(board.position_key, score, best_move.m16, depth, hashentry_kind);
