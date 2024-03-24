7edbd81c37db5ddb8dc1b941c5bf7855c5236004
abrni <45497598+abrni@users.noreply.github.com>
2024-03-24 22:31:29 +0100

    Improve build.rs

--- build.rs
+++ build.rs
@@ -2,8 +2,11 @@ use std::process::Command;
 
 fn main() {
     let _ = Command::new("cargo")
-        .args(["run", "--target-dir", "./target"])
+        .args(["run", "--target-dir", "../temp_target"])
         .current_dir("./tables_gen")
         .output()
         .unwrap();
+
+    println!("cargo:rerun-if-changed=tables_gen");
+    std::fs::remove_dir_all("./temp_target").unwrap();
 }
