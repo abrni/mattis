c9cd09e4e3f158e62d95c5d92356bdd3db19bfe5
abrni <45497598+abrni@users.noreply.github.com>
2024-05-06 16:03:58 +0200

    Fix mattis build
    - use tables_gen as a build-dependency, not as a binary

--- mattis/build.rs
+++ mattis/build.rs
@@ -1,12 +1,58 @@
-use std::process::Command;
+const OUT_DIR: &str = "../target/generated_tables";
+
+macro_rules! run_gen {
+    ($name:ident) => {{
+        let path = format!("{OUT_DIR}/{}", stringify!($name));
+        let path = std::path::Path::new(&path);
+
+        if !path.exists() {
+            let result = $name();
+            let bytes = bytemuck::bytes_of(&result);
+            std::fs::write(path, bytes).unwrap();
+        }
+    }};
+}
 
 fn main() {
-    let _ = Command::new("cargo")
-        .args(["run", "--target-dir", "../temp_target"])
-        .current_dir("./tables_gen")
-        .output()
-        .unwrap();
+    let dir_path = std::path::Path::new(OUT_DIR);
+    if !dir_path.exists() {
+        std::fs::create_dir(dir_path).unwrap();
+    }
 
-    println!("cargo:rerun-if-changed=tables_gen");
-    std::fs::remove_dir_all("./temp_target").unwrap();
+    {
+        use tables_gen::*;
+        run_gen!(zobrist_piece_keys);
+        run_gen!(zobrist_color_key);
+        run_gen!(zobrist_castle_keys);
+        run_gen!(zobrist_en_passant_keys);
+
+        run_gen!(file_bitboards);
+        run_gen!(not_file_bitboards);
+
+        run_gen!(rank_bitboards);
+        run_gen!(not_rank_bitboards);
+
+        run_gen!(border);
+
+        run_gen!(white_pawn_passed_masks);
+        run_gen!(black_pawn_passed_masks);
+        run_gen!(isolated_pawn_masks);
+
+        run_gen!(knight_move_patterns);
+        run_gen!(king_move_patterns);
+        run_gen!(rook_move_patterns);
+        run_gen!(bishop_move_patterns);
+
+        run_gen!(rook_magic_bit_count);
+        run_gen!(bishop_magic_bit_count);
+
+        run_gen!(rook_magic_masks);
+        run_gen!(bishop_magic_masks);
+
+        run_gen!(rook_magics);
+        run_gen!(bishop_magics);
+    }
+
+    println!("cargo:rerun-if-changed=../target/generated_tables");
+    println!("cargo:rerun-if-changed=../tables_gen");
 }
